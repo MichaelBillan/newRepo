407b2aee96357c2e2e5c639f3723d64cfff1e06a
mohammad.zedany@e.braude.ac.il <Lenovo@10.10.13.152>
2025-03-23 12:03:36 +0200

    delete nothing

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -3,7 +3,7 @@ public class SharedData
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
-	private final int b; // nothing
+	private final int b;
 	
 	public SharedData(int[] array, int b) {
 		
